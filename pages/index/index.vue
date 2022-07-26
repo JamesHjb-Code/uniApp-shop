cf65d6a95e39b66a977283514440706aec7fee68
unknown <907310116@qq.com>
2022-07-26 11:27:31 +0800

    uniapp底部菜单栏

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,5 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
 	</view>
 </template>
 
@@ -29,24 +25,5 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
 	}
 </style>
