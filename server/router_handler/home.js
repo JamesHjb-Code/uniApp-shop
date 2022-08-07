66c1f7444ecab1ccfea3c88c7e4f8fd8168b133e
James-notebookComputer <907310116@qq.com>
2022-08-07 15:23:15 +0800

    更新查询商品列表（分页）

--- server/router_handler/home.js
+++ server/router_handler/home.js
@@ -22,12 +22,22 @@ exports.getSlideShow = (req,res) =>{
 		})
 	})
 }
-// 获取热门商品数据
+// 获取热门商品数据(查询分页)
 exports.getHotShop = (req,res) =>{
+	// 获取从客户端数据过来，同时转换整数
+	const params = {
+		page:parseInt(req.query.page),
+		count:parseInt(req.query.count)
+	}
+	// 验证是否数据存在，否则给默认的值
+	if(!params.page) params.page = 1
+	if(!params.count) params.count = 10
+	// 计算分页
+	const handlePage = (params.page-1)*params.count;
 	// 定义sql语句
-	const sql = `select * from hotshop`
+	const sql = `select * from hotshop limit ?,?`
 	// 调用db.query()执行sql语句
-	db.query(sql,(err,results)=>{
+	db.query(sql,[handlePage,params.count],(err,results)=>{
 		if(err) return res.lose(err)
 		res.send({
 			message:'获取热门商品数据成功!',
