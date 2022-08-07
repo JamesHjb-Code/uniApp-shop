6a6b58fd66b3c84abbbadd0923af0dd45b31b0f1
James-notebookComputer <907310116@qq.com>
2022-08-07 17:19:58 +0800

    新增下拉刷新和上啦触底

--- pages/shop/shop.vue
+++ pages/shop/shop.vue
@@ -1,6 +1,8 @@
+
 <template>
 	<view class="shop">
 		<shop-list :shopList="shopList"></shop-list>
+		<view class="bottom-text" v-if="isBottom"><text>{{bottomText}}</text></view>
 	</view>
 </template>
 
@@ -12,16 +14,35 @@ import shopList from '@/components/shop-list/shop-list.vue'
 		},	
 		data() {
 			return {
-				shopList:[],
-				currentPage:1,
+				shopList:[], // 商品列表
+				currentPage:1, // 页数
+				isBottom:false, //是否显示底部
+				bottomText:'Have been to'
 			}
 		},
 		onLoad() {
 			this.getShopList()
 		},
+		onReachBottom(){
+			// 触底了底部
+			if(this.shopList.length < this.currentPage*10) return this.isBottom = true
+			this.currentPage++
+			this.getShopList()
+		},
+		onPullDownRefresh() {
+			// 下拉刷新
+			this.currentPage = 1
+			this.shopList = []
+			this.isBottom = false
+			setTimeout(()=>{
+				this.getShopList(()=>{
+					uni.stopPullDownRefresh()
+				})
+			},1500)
+		},
 		methods: {
 			// 获取商品列表数据
-			async getShopList(){
+			async getShopList(callBack){
 				const params = {
 					page:this.currentPage,
 					count:10
@@ -31,10 +52,11 @@ import shopList from '@/components/shop-list/shop-list.vue'
 					data:params
 				})
 				if(res.data.success){
-					this.shopList = res.data.result
+					this.shopList=[...this.shopList,...res.data.result]
 				}else{
 					this.shopList = []
 				}
+				callBack && callBack()
 			}
 		}
 	}
@@ -43,5 +65,11 @@ import shopList from '@/components/shop-list/shop-list.vue'
 <style lang="scss" scoped> 
 .shop{
 	background: #e5e5e5;
+	.bottom-text{
+		width:100vw;
+		height:50rpx;
+		padding:10rpx 0;
+		text-align: center;
+	}
 }
 </style>
