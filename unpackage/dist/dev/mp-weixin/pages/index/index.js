22c63da78250a03f0b4c8f85b94e1a9d2e72cd84
James-notebookComputer <907310116@qq.com>
2022-08-07 15:24:05 +0800

    更新数据

--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -104,6 +104,29 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "recyclableRender", function() { return recyclableRender; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "components", function() { return components; });
 var components
+try {
+  components = {
+    shopList: function() {
+      return __webpack_require__.e(/*! import() | components/shop-list/shop-list */ "components/shop-list/shop-list").then(__webpack_require__.bind(null, /*! @/components/shop-list/shop-list.vue */ 48))
+    }
+  }
+} catch (e) {
+  if (
+    e.message.indexOf("Cannot find module") !== -1 &&
+    e.message.indexOf(".vue") !== -1
+  ) {
+    console.error(e.message)
+    console.error("1. 排查组件名称拼写是否正确")
+    console.error(
+      "2. 排查组件是否符合 easycom 规范，文档：https://uniapp.dcloud.net.cn/collocation/pages?id=easycom"
+    )
+    console.error(
+      "3. 若组件不符合 easycom 规范，需手动引入，并在 components 中注册该组件"
+    )
+  } else {
+    throw e
+  }
+}
 var render = function() {
   var _vm = this
   var _h = _vm.$createElement
@@ -139,42 +162,35 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _regenerator = _interopRequireDefault(__webpack_require__(/*! ./node_modules/@babel/runtime/regenerator */ 19));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {try {var info = gen[key](arg);var value = info.value;} catch (error) {reject(error);return;}if (info.done) {resolve(value);} else {Promise.resolve(value).then(_next, _throw);}}function _asyncToGenerator(fn) {return function () {var self = this,args = arguments;return new Promise(function (resolve, reject) {var gen = fn.apply(self, args);function _next(value) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);}function _throw(err) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);}_next(undefined);});};} //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-var _default =
+/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _regenerator = _interopRequireDefault(__webpack_require__(/*! ./node_modules/@babel/runtime/regenerator */ 19));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {try {var info = gen[key](arg);var value = info.value;} catch (error) {reject(error);return;}if (info.done) {resolve(value);} else {Promise.resolve(value).then(_next, _throw);}}function _asyncToGenerator(fn) {return function () {var self = this,args = arguments;return new Promise(function (resolve, reject) {var gen = fn.apply(self, args);function _next(value) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);}function _throw(err) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);}_next(undefined);});};}var shopList = function shopList() {__webpack_require__.e(/*! require.ensure | components/shop-list/shop-list */ "components/shop-list/shop-list").then((function () {return resolve(__webpack_require__(/*! @/components/shop-list/shop-list.vue */ 48));}).bind(null, __webpack_require__)).catch(__webpack_require__.oe);};var _default =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 {
+  components: {
+    shopList: shopList },
+
   data: function data() {
     return {
       swipers: [], // 轮播图数据
@@ -182,25 +198,29 @@ var _default =
       {
         id: 1,
         text: '小斌超市',
-        icon: 'icon-ziyuan' },
+        icon: 'icon-ziyuan',
+        path: '/pages/shop/shop' },
 
       {
         id: 2,
         text: '联系我们',
-        icon: 'icon-guanyuwomen' },
+        icon: 'icon-guanyuwomen',
+        path: '/pages/contact/contact' },
 
       {
         id: 3,
         text: '社区图片',
-        icon: 'icon-tupian' },
+        icon: 'icon-tupian',
+        path: '/pages/pics/pics' },
 
       {
         id: 4,
         text: '学习视频',
-        icon: 'icon-shipin' }],
+        icon: 'icon-shipin',
+        path: 'pages/videos/videos' }],
 
 
-      hotGoodsList: [] // 热门商品列表
+      shopList: [] // 热门商品列表
     };
   },
   onLoad: function onLoad() {
@@ -225,11 +245,18 @@ var _default =
                     url: '/home/hotshop' }));case 2:res = _context2.sent;
 
                 if (res.data.success) {
-                  _this2.hotGoodsList = res.data.result;
+                  _this2.shopList = res.data.result;
                 } else {
-                  _this2.hotGoodsList = [];
+                  _this2.shopList = [];
                 }case 4:case "end":return _context2.stop();}}}, _callee2);}))();
+    },
+    // 点击跳转导航函数
+    clickNavItem: function clickNavItem(path) {
+      uni.navigateTo({
+        url: path });
+
     } } };exports.default = _default;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"]))
 
 /***/ }),
 /* 19 */,
