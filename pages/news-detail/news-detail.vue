597c3fff7bdfe4aa4e7c5849422cf26e68297572
HuangJianbin <907310116@qq.com>
2022-08-11 17:36:20 +0800

    新增新闻详情页面

---
+++ pages/news-detail/news-detail.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="news-detail">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.news-detail{
+	
+}
+</style>
