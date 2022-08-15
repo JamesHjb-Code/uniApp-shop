df48dad9d5275795253bb3d66b5d4b873d2d0505
HuangJianbin <907310116@qq.com>
2022-08-15 17:30:32 +0800

    添加新闻详情

--- pages/news-detail/news-detail.vue
+++ pages/news-detail/news-detail.vue
@@ -1,6 +1,13 @@
 <template>
 	<view class="news-detail">
-		
+		<text class="title">标题</text>
+		<view class="info">
+			<text>发表时间：</text>
+			<text>浏览：</text>
+		</view>
+		<view class="content">
+			内容
+		</view>
 	</view>
 </template>
 
@@ -12,13 +19,27 @@
 			}
 		},
 		methods: {
-			
+			getNewsDetail(){
+				this.$myRuquest({
+					url:'/api/getnew/'
+				})
+			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
 .news-detail{
-	
+	font-size:30rpx;
+	.title{
+		text-align: center;
+		width:100%;
+		display: block;
+		margin:20rpx 0;
+	}
+	.info{
+		display: flex;
+		justify-content: space-between;
+	}
 }
 </style>
