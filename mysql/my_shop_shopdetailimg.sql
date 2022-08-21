2ebb6bb5ba2be8e96e4bbcb460b16cb2a0914092
James-notebookComputer <907310116@qq.com>
2022-08-21 21:38:02 +0800

    提交更新mysql

--- mysql/my_shop_shopdetailimg.sql
+++ mysql/my_shop_shopdetailimg.sql
@@ -16,29 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `slideimg`
+-- Table structure for table `shopdetailimg`
 --
 
-DROP TABLE IF EXISTS `slideimg`;
+DROP TABLE IF EXISTS `shopdetailimg`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `slideimg` (
-  `ID` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `shopdetailimg` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `img` varchar(255) NOT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  `goods_id` int DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `shop_Id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `slideimg`
+-- Dumping data for table `shopdetailimg`
 --
 
-LOCK TABLES `slideimg` WRITE;
-/*!40000 ALTER TABLE `slideimg` DISABLE KEYS */;
-INSERT INTO `slideimg` VALUES (1,'/static/home/banner1.png','/pages/goods_detail/main',1),(2,'/static/home/banner2.png','/pages/goods_detail/main',2),(3,'/static/home/banner3.png','/pages/goods_detail/main',3);
-/*!40000 ALTER TABLE `slideimg` ENABLE KEYS */;
+LOCK TABLES `shopdetailimg` WRITE;
+/*!40000 ALTER TABLE `shopdetailimg` DISABLE KEYS */;
+INSERT INTO `shopdetailimg` VALUES (1,'/static/shopDetailImg/apple12.jpg',1),(2,'/static/shopDetailImg/apple12-1.jpg',1),(3,'/static/shopDetailImg/xiaomi11qingchun.jpg',2),(4,'/static/shopDetailImg/xiaomi11qingchun-1.jpg',2),(5,'/static/shopDetailImg/xiaomi11pro.jpg',3),(6,'/static/shopDetailImg/xiaomi11pro-1.jpg',3),(7,'/static/shopDetailImg/huaweimate30.jpg',4),(8,'/static/shopDetailImg/huaweimate30-1.jpg',4),(9,'/static/shopDetailImg/huaweinova8.jpg',5),(10,'/static/shopDetailImg/huaweinove8-1.jpg',5),(11,'/static/shopDetailImg/huaweimate30.jpg',6),(12,'/static/shopDetailImg/huaweimate30-1.jpg',6),(13,'/static/shopDetailImg/vivos9e.jpg',7),(14,'/static/shopDetailImg/xiaomi11pro-1.jpg',7),(15,'/static/shopDetailImg/oppo.jpg',8),(16,'/static/shopDetailImg/oppo-1.jpg',8),(17,'/static/shopDetailImg/apple12.jpg',9),(18,'/static/shopDetailImg/apple12-1.jpg',9);
+/*!40000 ALTER TABLE `shopdetailimg` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
